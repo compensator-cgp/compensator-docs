d14c65e1d91d661faa8a5f9c7c68a7b76e92e0b5
camconrad <camconrad@gmail.com>
2025-07-14 10:26:28 -0600

    redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,12 @@
-import React, { useEffect } from 'react';
-import { useHistory } from '@docusaurus/router';
+import * as React from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
-  const history = useHistory();
-
   useEffect(() => {
-    // Redirect to the introduction page
-    history.replace('/overview/intro');
-  }, [history]);
+    // Instant redirect to the introduction page with no loading text
+    window.location.replace('/overview/intro');
+  }, []);
 
-  return (
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh',
-      fontSize: '18px'
-    }}>
-      Redirecting to introduction...
-    </div>
-  );
+  // Return null to show nothing while redirecting
+  return null;
 } 
